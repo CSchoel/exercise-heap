ec4eb36345b9d91652009ac38b2995fc4781c537
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-03-02 19:15:59 +0100

    fixes use of subparsers

--- scripts/import/porty.py
+++ scripts/import/porty.py
@@ -6,14 +6,12 @@ import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Porty", description="Your friendly import bot")
+    subparsers = parser.add_subparsers()
 
     # ./porty.py import
-    import_parser=argparse.ArgumentParser()
+    import_parser = subparsers.add_parser("import")
 
     # ./porty.py update
-    update_parser=argparse.ArgumentParser()
+    update_parser = subparsers.add_parser("update")
 
-    subparsers = parser.add_subparsers()
-    subparsers.add_parser("import", import_parser)
-    subparsers.add_parser("update", update_parser)
     args = parser.parse_args(sys.argv[1:])
