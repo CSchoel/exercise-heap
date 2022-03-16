5a25f5bd938532400ba1b657b5f951550a24ca3d
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-03-16 20:09:27 +0100

    textracts tag names and replaces dashes with spaces

--- scripts/metadata/create_tagnames.py
+++ scripts/metadata/create_tagnames.py
@@ -5,11 +5,15 @@ Creates list of tag names from Stack Exchange Data Dump
 
 import argparse
 import sys
-import lxml
+from pathlib import Path
+import lxml.etree
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Tagname creator", description=__doc__)
     parser.add_argument("file")
     parser.add_argument("--mincount", "-c", help="minimum count required to add tag name", default=0, type=int)
     args = parser.parse_args(sys.argv[1:])
-    print(args)
+    data = lxml.etree.parse(args.file)
+    tags = data.xpath(f"//row[@Count>{args.mincount}]/@TagName")
+    tags = [x.replace("-", " ") for x in tags]
+    print(tags)
