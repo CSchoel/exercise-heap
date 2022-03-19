3c25d9425e8c9131e7f7cddbfa82e5286b3c8bc6
Christopher Schölzel <christopher.schoelzel@gmx.net>
2022-03-19 17:02:02 +0100

    checks out branch before doing git diff

--- scripts/import/porty.py
+++ scripts/import/porty.py
@@ -178,7 +178,8 @@ def find_exfile(event, dry=False) -> Tuple[Path, str]:
 
     base = pull["base"]["ref"]
     head = pull["head"]["ref"]
-    out = maybe_run(["git", "diff", f"{base}..origin/{head}", "--name-only"], dry=dry, capture_output=True)
+    maybe_run(["git", "checkout", head], dry=dry)
+    out = maybe_run(["git", "diff", f"{base}..{head}", "--name-only"], dry=dry, capture_output=True)
     if dry:
         out = "exercises/this/is_a/dry/run.md"
     filename = [x for x in out.splitlines() if re.match(r"exercises\/.+\/.+\/.+\/.+\.md", x)]
