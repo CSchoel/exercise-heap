f460fd90a4223d50200a98652a3c7ce33ca66816
Christopher Schölzel <christopher.schoelzel@gmx.net>
2023-01-27 20:15:48 +0100

    adds base class for translator

--- scripts/translate/translate_exercise_text.py
+++ scripts/translate/translate_exercise_text.py
@@ -1,6 +1,7 @@
 """Translate exercise text into another language using language models."""
 
 from pathlib import Path
+from typing import Dict, Any
 
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
@@ -9,6 +10,35 @@ from nltk.tokenize import sent_tokenize
 
 
 class Translator:
+    def __init__(self, model_name: str, tokenizer_args: Dict[str, Any] = {}, model_args: Dict[str, Any] = {}):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, **tokenizer_args)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, **model_args)
+
+    def translate(self, text: str, **extra_args) -> str:
+        """Translate a text.
+
+        Args:
+            text (str): text that should be translated
+            extra_args: arguments passed on to the generate() function
+
+        Returns:
+            str: translated text
+        """
+        inputs = self.tokenizer(text, return_tensors="pt")
+        translated_tokens = self.model.generate(**inputs, **extra_args)
+        res = self.tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+        return res
+
+
+class HelsinkiNLPTranslator(Translator):
+    """Translate text from one language into another using an opus-mt model from HelsinkiNLP."""
+
+    def __init(self, source_lang: str = "de", target_lang: str = "en"):
+        model_name = f"Helsinki-NLP/opus-mt-{source_lang}-{target_lang}"
+        super().__init__(model_name)
+        
+
+class NLLBTranslator(Translator):
     """Translate text from one language into another."""
 
     def __init__(
@@ -23,28 +53,22 @@ class Translator:
             source_lang (str, optional): Language (BCP 47) of source texts \
                 that will be translated with this model
         """
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            model_name, use_auth_token=True, source_lang=source_lang
-        )
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, use_auth_token=True)
+        super().__init__(model_name, tokenizer_args={"source_lang": source_lang})
 
-    def translate(self, text: str, target_lang="deu_Latn"):
+    def translate(self, text: str, target_lang="deu_Latn") -> str:
         """Translate a text to a target language.
 
         Args:
             text (str): target language as defined in
-            target_lang (str, optional): _description_. Defaults to "deu_Latn".
+            target_lang (str, optional): target language (BCP 47). Defaults to "deu_Latn".
 
         Returns:
-            _type_: _description_
+            str: translated sentence
         """
-        inputs = self.tokenizer(text, return_tensors="pt")
-
-        translated_tokens = self.model.generate(
-            **inputs, forced_bos_token_id=self.tokenizer.lang_code_to_id[target_lang], max_length=30
+        return super().translate(
+            text,
+            forced_bos_token_id=self.tokenizer.lang_code_to_id[target_lang]
         )
-        res = self.tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
-        return res
 
 
 def translate_exercise(path: str | Path, from_language="de_Latn", to_language="eng_Latn"):
@@ -55,7 +79,7 @@ def translate_exercise(path: str | Path, from_language="de_Latn", to_language="e
         from_language (str, optional): Source language. Defaults to "de_Latn".
         to_language (str, optional): Target language. Defaults to "eng_Latn".
     """
-    translator = Translator(
+    translator = NLLBTranslator(
         model_name="facebook/nllb-200-distilled-600M", source_lang=from_language
     )
     with exercise_editing(Path(path), dry_run=True) as ex:
