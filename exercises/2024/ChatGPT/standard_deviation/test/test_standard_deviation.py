93b8f3d554ac206ecc5dd485c84b86e9b04f1e49
Christopher Schölzel <christopher.schoelzel@gmx.net>
2024-03-24 18:58:31 +0100

    fixes expected value for single input

--- exercises/2024/ChatGPT/standard_deviation/test/test_standard_deviation.py
+++ exercises/2024/ChatGPT/standard_deviation/test/test_standard_deviation.py
@@ -3,8 +3,9 @@ import math
 
 from standard_deviation import compute_standard_deviation
 
+
 class TestStandardDeviation(unittest.TestCase):
-    
+
     def test_standard_deviation(self):
         data = [2.0, 4.0, 4.0, 4.0, 5.0, 5.0, 7.0, 9.0]
         result = compute_standard_deviation(data)
@@ -18,12 +19,13 @@ class TestStandardDeviation(unittest.TestCase):
     def test_standard_deviation_single_value(self):
         data = [5.0]
         result = compute_standard_deviation(data)
-        self.assertEqual(result, 0.0)
+        self.assertEqual(result, float("inf"))
 
     def test_standard_deviation_negative_values(self):
         data = [-2.0, -4.0, -4.0, -4.0, -5.0, -5.0, -7.0, -9.0]
         result = compute_standard_deviation(data)
         self.assertAlmostEqual(result, 2.138, places=3)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
